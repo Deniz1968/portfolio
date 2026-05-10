fec02f692a56937c050a33094a5b55f079bb25f5
Deniz Çetin <ddenizcetinn@outlook.com>
2026-05-11 00:12:16 +0300

    Create 1index.php

---
+++ 1index.php
@@ -0,0 +1,106 @@
+<?php
+
+$english_quiz = [
+    [
+        "question" => "Where ___ you go on your last vacation?",
+        "options" => ["do", "did", "are", "were"],
+        "answer" => "did"
+    ],
+    [
+        "question" => "I ___ a student at the university.",
+        "options" => ["is", "am", "are", "be"],
+        "answer" => "am"
+    ],
+    [
+        "question" => "How ___ sugar do you want in your coffee?",
+        "options" => ["many", "much", "long", "often"],
+        "answer" => "much"
+    ],
+    [
+        "question" => "Listen! My sister ___ the piano in the living room.",
+        "options" => ["plays", "play", "is playing", "played"],
+        "answer" => "is playing"
+    ],
+    [
+        "question" => "What time ___ the supermarket close?",
+        "options" => ["do", "is", "does", "are"],
+        "answer" => "does"
+    ],
+    [
+        "question" => "I'm bored. ___ we go to the cinema tonight?",
+        "options" => ["Shall", "Will", "Do", "Are"],
+        "answer" => "Shall"
+    ],
+    [
+        "question" => "This is my ___ car. It's blue.",
+        "options" => ["father", "fathers", "father's", "fathers'"],
+        "answer" => "father's"
+    ],
+    [
+        "question" => "Have you ___ been to London?",
+        "options" => ["never", "ever", "yet", "already"],
+        "answer" => "ever"
+    ],
+    [
+        "question" => "I usually ___ up at 7 o'clock.",
+        "options" => ["get", "gets", "getting", "got"],
+        "answer" => "get"
+    ],
+    [
+        "question" => "Look at those clouds! It ___ rain.",
+        "options" => ["is going to", "goes to", "will be", "is"],
+        "answer" => "is going to"
+    ],
+    [
+        "question" => "___ you like some orange juice?",
+        "options" => ["Do", "Can", "Would", "Are"],
+        "answer" => "Would"
+    ],
+    [
+        "question" => "My brother is ___ than me.",
+        "options" => ["tall", "taller", "tallest", "more tall"],
+        "answer" => "taller"
+    ],
+    [
+        "question" => "We ___ a great movie yesterday.",
+        "options" => ["see", "sees", "saw", "seen"],
+        "answer" => "saw"
+    ],
+    [
+        "question" => "There ___ any milk in the fridge.",
+        "options" => ["isn't", "aren't", "is", "any"],
+        "answer" => "isn't"
+    ],
+    [
+        "question" => "Can you speak English ___?",
+        "options" => ["good", "well", "slow", "easy"],
+        "answer" => "well"
+    ],
+    [
+        "question" => "I always go to work ___ bus.",
+        "options" => ["with", "on", "by", "in"],
+        "answer" => "by"
+    ],
+    [
+        "question" => "What is the ___ month of the year?",
+        "options" => ["hot", "hotter", "hottest", "more hot"],
+        "answer" => "hottest"
+    ],
+    [
+        "question" => "___ you finish your homework last night?",
+        "options" => ["Do", "Are", "Did", "Were"],
+        "answer" => "Did"
+    ],
+    [
+        "question" => "She ___ like watching football on TV.",
+        "options" => ["don't", "doesn't", "isn't", "not"],
+        "answer" => "doesn't"
+    ],
+    [
+        "question" => "Where is Deniz? He is ___ the kitchen.",
+        "options" => ["on", "at", "under", "in"],
+        "answer" => "in"
+    ]
+];
+
+?>
